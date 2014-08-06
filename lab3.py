c23a653f24142ed105769f3c7427dcc7f2f4e5d1
Joshua Boston <jboston@wustl.edu>
2014-08-06 15:14:04 -0500

    Lab 3 revised

--- lab3.py
+++ lab3.py
@@ -29,37 +29,7 @@ def reversewords(txt):
   
   return final_reverse
 
-  #return ' '.join(str(word_list) for x in word_list)
 
-
-  
-#  new_text = ""
-#  reversed_sentences = []
-    
-#  tmp = txt.replace("?", ".")
-#  tmp = tmp.replace("!", ".")
-#  sentences = tmp.split(". ")
-#  sentences = [s.strip() for s in sentences if len(s.strip()) > 0]
-  
-#  last_sentence = sentences[len(sentences) - 1]
-#  if last_sentence[len(last_sentence) - 1] == ".":
-#    sentences[len(sentences) - 1] = last_sentence[0:len(last_sentence)-1]
-  
-#  for sentence in sentences:
-#    words = sentence.split()
-#    words.reverse()
-#    reversed_sentence = ""
-#    for word in words:
-#      reversed_sentence += word
-#      reversed_sentence += " "
-#    reversed_sentences.append(reversed_sentence[0:(len(reversed_sentence)-1)])
-  
-#  for sentence in reversed_sentences:
-#    if len(sentence) > 0:
-#      new_text += sentence
-#      new_text += ". "
-    
-#  return new_text
   
 def reversewordletters(txt):
   if isinstance(txt, str) == False:
